067bc3186677a254931c6fb1bc794734aee8f371
rodrigues0411 <jesus.rodrigues.aline@escola.pr.gov.br>
2025-06-09 08:30:08 -0300

    aumenta

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 document.querySelector('.parametro-senha__botao');
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-numeroSenha.textContent = 5; 
+
+let tamanhoSenha = 12;
+
+numeroSenha.textContent = tamanhoSenha; 
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 console.log(botoes);
+
+function diminuirTamanho(){
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha
+}
