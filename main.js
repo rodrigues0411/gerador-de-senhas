a314585fb660dbfd2163c8972a406c5d5475f644
rodrigues0411 <jesus.rodrigues.aline@escola.pr.gov.br>
2025-06-09 08:19:33 -0300

    ghds

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector('.parametro-senha__botao');
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 numeroSenha.textContent = 5; 
+
+const botoes = document.querySelector('.parametro-senha__botao');
+
+console.log(botoes);
