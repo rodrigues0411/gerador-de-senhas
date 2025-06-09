6731f28a5b67f221b67f6772498e38ddd87d6ff6
rodrigues0411 <jesus.rodrigues.aline@escola.pr.gov.br>
2025-06-09 08:20:54 -0300

    çlceqj

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document.querySelector('.parametro-senha__botao');
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 numeroSenha.textContent = 5; 
 
-const botoes = document.querySelector('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 console.log(botoes);
